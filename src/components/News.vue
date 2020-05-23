bed983c55c3e64c4b9d9d0d68ecae86e50cac372
Damoon <d.shahhosseini@yahoo.com>
2020-05-22 20:47:10 -0400

    The rendering works for now but some modifications should be made

---
+++ src/components/News.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="main-div">
+      <li class="news-item" v-for="news in this.newses" :key="news.title">
+        <NewsHolder v-bind:news="news" />
+      </li>
+  </div>
+</template>
+
+
+<script>
+import { articles } from './mock.json';
+import NewsHolder from './NewsHolder';
+export default {
+  name: "news",
+  data() {
+    return {
+      newses: articles
+    }
+  },
+  components: {
+    NewsHolder,
+  },
+};
+</script>
+
+<style scoped>
+    .main-div {
+      /* Grid */
+      display: grid;
+      grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+      grid-gap: 2rem;
+      grid-auto-flow: dense;
+      /* Padding */
+      padding: 1rem;
+    }
+    .news-item {
+      display: flex;
+      flex-direction: row; 
+    }
+</style>
