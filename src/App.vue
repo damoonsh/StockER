ba148eb2e683b447c617f15d511467a53fbff6d9
Damoon <d.shahhosseini@yahoo.com>
2020-05-23 09:56:47 -0400

    This should not need any further modfication

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <v-app>
+    <router-view></router-view>
     <v-content>
-      <Nav />
+      
     </v-content>
   </v-app>
 </template>
 
 <script>
-import Nav from './components/Nav';
-
 export default {
   name: 'App',
-
-  components: {
-    Nav,
-  },
 };
 </script>
+
+<style scoped>
+
+</style>
