e69dc11988d6cde59cc2ad844f4e584b12d60176
Damoon <d.shahhosseini@yahoo.com>
2020-05-22 21:21:12 -0400

    Searchbar: style and functionality should be added!

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="search-bar">
+        <v-text-field
+            class="search-field"
+            label="Search"
+            :outlined="true"
+            :loading="false"
+            :dense="true"
+            :solo="true"
+            counter="20"
+          ></v-text-field>
+    </div>
+</template>
+
+<style scoped>
+    .search-bar {
+        display: grid;
+        grid-template-columns: 1fr 1fr 2fr 1fr 1fr;
+    }
+
+    .search-field {
+        grid-column: 3/4;
+    }
+</style>
